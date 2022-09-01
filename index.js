3f9f2592d68078cc4a56d627f80514086ee7c934
Lauren Thomeczek Manning <laurenyasmine@gmail.com>
2022-09-01 11:58:00 -0500

    commit tree e0bfba32f96748c7a0c39290eb14f27fe9e5409c

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solutions in this file
+/*
+for (let age =30; age < 40; age++) {
+    console.log(`I'm ${age} years old.  Happy Birthday to me!`)
+}
+
+
+const gifts = ["teddy bear", "drone", "doll"]
+
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+        debugger
+    }
+    return gifts
+}
+
+wrapGifts(gifts)
+
+*/
+
+
+const thankYouCards = []
+
+function writeCards(name, eventName) {
+    for (let x=0; x < name.length; x++) {
+        thankYouCards[x] = `Thank you, ${name[x]}, for the wonderful ${eventName} gift!`
+        debugger
+    }
+    return thankYouCards
+}
+writeCards(["Guadelupe", "Ollie", "Aki"], "surprise")
+
+
+function countDown(x) {
+    let num = x;
+    while (num > -1) {
+        console.log(num--)
+    }
+}
+console.log(countDown(10))
